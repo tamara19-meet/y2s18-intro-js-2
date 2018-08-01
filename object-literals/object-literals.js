15133fd6db341ab51704aa131b6cbf60deb1ba8b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:16:16 +0300

    kyfahk

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
 // Create your 'me' object literal here!
+var me ={
+	firstName: "Tamara",
+	lastName:"Hashimeh",
+	favoriteFoods:["Potatoes, Pizza, Burger"],
+	age: 16,
+	image:"https://www.google.co.il/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj0yabxvMvcAhUSxIUKHf6aClIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.tesco.com%2Fgroceries%2Fen-GB%2Fproducts%2F253558183&psig=AOvVaw2TesDHM9GyU47TDiYnYXSQ&ust=1533199908388234"
+}
+document.write(JSON.stringify(me))
